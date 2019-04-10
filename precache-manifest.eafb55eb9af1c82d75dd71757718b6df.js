fc9f661b861d69e92376def35e182563d56f370a
Chris Weed <chris@workgrid.com>
2019-04-10 09:17:30 -0500

    🔥

--- precache-manifest.eafb55eb9af1c82d75dd71757718b6df.js
+++ precache-manifest.eafb55eb9af1c82d75dd71757718b6df.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6717e7ebe2d9b1611a11",
+    "revision": "752d006229c86697b33a",
     "url": "/micro-frames/static/css/main.5971a5e2.chunk.css"
   },
   {
-    "revision": "6717e7ebe2d9b1611a11",
-    "url": "/micro-frames/static/js/main.e86490ee.chunk.js"
+    "revision": "752d006229c86697b33a",
+    "url": "/micro-frames/static/js/main.4d7695f2.chunk.js"
   },
   {
     "revision": "896d128046ae0092aa02",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/micro-frames/static/js/2.136228cd.chunk.js"
   },
   {
-    "revision": "8d7c0d5c5cedf67920065de54cd1277f",
+    "revision": "557cabbc707b693ffb364cea7c13c3bb",
     "url": "/micro-frames/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/micro-frames/precache-manifest.cb4909db39b391fadd82408c3e779ddf.js"
+  "/micro-frames/precache-manifest.eafb55eb9af1c82d75dd71757718b6df.js"
 );
 
 workbox.clientsClaim();
